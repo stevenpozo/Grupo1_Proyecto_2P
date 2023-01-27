3acd6480f0baeabc75e4d5300049bef2bb94335f
Sebastian-Paucar <67701134+Sebastian-Paucar@users.noreply.github.com>
2023-01-27 02:01:18 -0500

    Add files via upload

---
+++ TERCER PARCIAL/5. CODIFICACION/GraficaTablero.h
@@ -0,0 +1,7 @@
+#pragma once
+
+
+char** LeerArchivo();
+void GenerarTablero();
+int Tamaniomatriz();
+void ColorearTabla(char );
--- TERCER PARCIAL/5. CODIFICACION/main.cpp
+++ TERCER PARCIAL/5. CODIFICACION/main.cpp
@@ -1,18 +1,11 @@
-/***********************************************************************
- * UFA-ESPE
- * Nombres: Steven Pozo,Quilumbaquin Jairo
- * Fecha de creacion: miércoles, 26 de octubre de 2022 19:06:30
- * Purpose: Implementation of the class Persona
- * Enunciado del problema: programar en datos abstractos para ingresar datos personales y calcular la edad
- ***********************************************************************/ 
-
 #include <iostream>
-#include "Persona.cpp"
-using namespace std;
+#include "ArchivoTablero.cpp"
+#include "GraficaTablero.cpp"
 
-int main(){
-	Persona persona1= Persona();
-	persona1.pedirDatos();
- 	persona1.calcularEdad();
- 	persona1.imprimirEdad();
+int main() {
+	
+	CrearArchivo();
+    GenerarTablero();
+
+    return 0;
 }
