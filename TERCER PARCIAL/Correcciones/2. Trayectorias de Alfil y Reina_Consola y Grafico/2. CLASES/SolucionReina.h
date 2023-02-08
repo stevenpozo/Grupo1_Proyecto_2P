8dde0382386cfc5d8e4a174701de4c1ee4342ed9
Sebastian-Paucar <67701134+Sebastian-Paucar@users.noreply.github.com>
2023-02-07 23:15:31 -0500

    Add files via upload

---
+++ TERCER PARCIAL/Correcciones/2. Trayectorias de Alfil y Reina_Consola y Grafico/2. CLASES/SolucionReina.h
@@ -0,0 +1,23 @@
+
+
+class SolucionReina
+{
+public:
+   char** creartableroArchivo_Reina(void);
+   void encerarTablero_Reina(void);
+   void mostrarTableroConsola_Reina(void);
+   bool crearArchivo_Reina(void);
+   char** leerArchivo_Reina(void);
+   void generarTrayectoria_Reina(int posicionX, int posicionY);
+   void generarPosicionesAleatorias_Reina();
+   void mostrarTableroArchivo_Reina(void);
+   void dibujarTablero_Reina();
+   void dibujarTableroArchivo_Reina();
+   void mostrarCoordenadas_Reina();
+   SolucionReina();
+   ~SolucionReina();
+
+protected:
+private:
+
+};
